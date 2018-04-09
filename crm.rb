8944ed4f675a74f6819c2358477461dba8648bfa
behrami <behram1993@gmail.com>
2018-04-09 15:49:03 -0400

    added custom 404 error

--- crm.rb
+++ crm.rb
@@ -2,16 +2,23 @@ require_relative 'contact.rb'
 require 'sinatra'
 
 get '/' do
-  @contactlist = Contact.all
-  @contactnum= Contact.all.size
-  erb :index
+  redirect '/contacts'
 end
 
-# get '/contacts' do
-#   @contactlist = Contact.all
-#   @contactnum= Contact.all.size
-#   erb :contacts
-# end
+get '/contacts' do
+  @contactlist = Contact.all
+  @contactnum= Contact.all.size
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+   raise Sinatra::NotFound
+  end
+end
 
 get '/about' do
   erb :about
